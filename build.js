94e9c620e69bf9eb8cad359992be8fc4bd40f429
fufroom <hello.fufroom@gmail.com>
2024-03-12 15:17:39 -0400

    initial commit

---
+++ build.js
@@ -0,0 +1,34 @@
+const fs = require('fs-extra');
+const Handlebars = require('handlebars');
+const path = require('path');
+
+async function generateHTML() {
+  // Load and compile the template from index.html
+  const templatePath = path.join(__dirname, 'index.html');
+  const source = await fs.readFile(templatePath, 'utf8');
+  const template = Handlebars.compile(source);
+
+  // Load your game data JSON
+  let gameData = await fs.readJson(path.join(__dirname, 'game_data.json'));
+
+  // Enhance each game object with an img property
+  gameData.forEach(game => {
+    game.img = `img/${game.gameName.replace(/\s+/g, '-').toLowerCase()}.png`;
+  });
+
+  // Organize games by category according to the new rules
+  const categorizedGames = {
+    released: gameData.filter(game => game.released && typeof game.jam === 'undefined'),
+    inProgress: gameData.filter(game => !game.released && typeof game.jam === 'undefined'),
+    jams: gameData.filter(game => game.released && typeof game.jam !== 'undefined')
+  };
+
+  // Generate final HTML
+  const html = template({ games: categorizedGames });
+
+  // Write the generated HTML to a file
+  await fs.writeFile(path.join(__dirname, 'dist/index.html'), html);
+  console.log('Index.html generated successfully!');
+}
+
+generateHTML().catch(console.error);
---
+++ scripts.js
@@ -0,0 +1,32 @@
+function getRandomBoldColor() {
+    const boldColors = [
+        "#ef8622", // Orange
+        "#fdc20f", // Yellow-orange
+        "#a1cd3a", // Green
+        "#49bea7", // Teal
+        "#6dcbd2", // Light-teal
+        "#27bfd1", // Bright-blue
+        "#6666ae", // Purple-blue
+        "#85458a", // Purple
+        "#b86bac", // Light-purple
+        "#e84699", // Pink
+        "#e3088c", // Fuschia
+    ];
+    const randomIndex = Math.floor(Math.random() * boldColors.length);
+    return boldColors[randomIndex];
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    function changeLinkColors() {
+        const links = document.querySelectorAll('a'); 
+        links.forEach(link => {
+            const randomColor = getRandomBoldColor(); 
+            link.style.color = randomColor; 
+        });
+    }
+
+    changeLinkColors();
+    
+    setInterval(changeLinkColors, 20000);
+});
